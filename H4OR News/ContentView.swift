781fe78d9990770970acfe317f35d0c4527f64e1
daaleksandrov <daaleksandrov@domclick.ru>
2020-02-15 16:22:04 +0300

    Initial Commit

---
+++ H4OR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4OR News
+//
+//  Created by Александров Денис Александрович on 15.02.2020.
+//  Copyright © 2020 Александров Денис Александрович. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
